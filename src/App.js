baa09fff3fa48eb47a9da353ae5a1e961dc36895
elricforser <157776168+elricforser@users.noreply.github.com>
2024-01-26 12:53:27 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import "./styles.css";
+
+import React from 'react';
+import axios from 'axios';
+
+export default function App() {
+  const fetchTableNames = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/get-table-names');
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error fetching table names', error);
+    }
+  }
+};
+
+export default function App() {
+  return (
+    <div className="position-fixed bg-primary h-100 w-100">
+      <div className="position-absolute top-0 end-0">
+        <button
+          className="btn btn-success d-flex justify-content-center align-items-center m-3"
+          style={{ height: "50px", width: "200px" }}
+          onClick={fetchTableNames}
+        >
+          ABSOLUTES
+        </button>
+      </div>
+      <div className="position-absolute top-50 start-50 translate-middle">
+        <div
+          className="bg-success text-light d-flex justify-content-center align-items-center"
+          style={{ height: "50px", width: "200px" }}
+        >
+          LOGO CENTER
+        </div>
+        <span className="d-flex justify-content-center text-light">
+          Click and Log in
+        </span>
+      </div>
+    </div>
+  );
+}
